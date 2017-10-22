2373558d566102d4fc09689897266f15c938469c
Rich Wright <rchwrght@gmail.com>
2017-10-22 17:09:53 +0200

    Creating Client Logging

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost() {
+        StartHost();
+        Debug.Log(Time.timeSinceLevelLoad + " Starting Host");
+    }
+
+    public override void OnStartHost() {
+        Debug.Log(Time.timeSinceLevelLoad + " Host Started");
+    }
+
+    public override void OnStartClient(NetworkClient myClient) {
+        Debug.Log(Time.timeSinceLevelLoad + " Client start requested");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn) {
+        Debug.Log(Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+    }
+}
